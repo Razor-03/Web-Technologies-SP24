f37a4bc1edabc9a9820c554cbf28a536d55b6439
Razor-03 <hraza0926@gmail.com>
2024-03-20 23:23:51 +0500

    some get routes added

--- Practice/Express/FirstApp/index.js
+++ Practice/Express/FirstApp/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res) => {
-    res.send("<h1>Hello World</h1>");
+// app.use((req, res) => {
+//     res.send("<h1>Hello World</h1>");
+// });
+
+app.get("/", (req, res) => {
+    res.send("<h1>Root route</h1>");
+});
+
+app.get(`/r/:subreddit`, (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`<h1>Browsing the ${subreddit} subreddit</h1>`);
+});
+
+app.get(`/r/:subreddit/:postId`, (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`<h1>Viewing Post ID: ${postId} on the ${subreddit} subreddit</h1>`);
 });
 
 app.listen(3000, () => {
