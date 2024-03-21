65c76f3eeae85fe445afdb0631d7f07e00c15f78
Razor-03 <hraza0926@gmail.com>
2024-03-22 01:02:21 +0500

    subreddit style dynamic markup

--- Practice/Express/Templating_Demo/index.js
+++ Practice/Express/Templating_Demo/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require("path")
+const redditData = require("./data.json");
 
 app.set('view engine', 'ejs');
 app.set(path.join(__dirname, '/views'));
@@ -13,7 +14,10 @@ app.get('/', (req, res) => {
 
 app.get('/r/:subreddit', (req, res) => {
     const { subreddit } = req.params;
-    res.render('subreddit', { subreddit })
+    // console.log(redditData);
+    const data = redditData[subreddit];
+    // console.log(data);
+    res.render('subreddit', { ...data });
 });
 
 app.listen(3000, () => {
