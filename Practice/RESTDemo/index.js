7ac1978b87dac3bda475178fbaf9eeb6d3bafe99
Razor-03 <hraza0926@gmail.com>
2024-03-26 00:29:37 +0500

    post form added

--- Practice/RESTDemo/index.js
+++ Practice/RESTDemo/index.js
@@ -1,26 +1,31 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.set('view engine', 'ejs');
-app.set(views, path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'views'));
 
 const comments = [
     {
+        id: uuid(),
         username: 'Todd',
         comment: 'lol that is so funny!'
     },
     {
+        id: uuid(),
         username: 'Skyler',
         comment: 'I like to go birdwatching with my dog'
     },
     {
+        id: uuid(),
         username: 'Sk8erBoi',
         comment: 'Plz delete your account, Todd'
     },
     {
+        id: uuid(),
         username: 'onlysayswoof',
         comment: 'woof woof woof'
     }
@@ -29,6 +34,12 @@ const comments = [
 app.get('/comments', (req, res) => {
     res.render('comments/index', { comments });
 })
+
+app.get('/comments/:id/edit', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === id);
+    res.render('comments/edit', { comment });
+});
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
