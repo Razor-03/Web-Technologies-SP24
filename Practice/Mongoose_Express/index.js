ab9f1d427ef0a035780fb36f30e8b6669ed9906a
Razor-03 <hraza0926@gmail.com>
2024-03-27 01:01:44 +0500

    edit route completed

--- Practice/Mongoose_Express/index.js
+++ Practice/Mongoose_Express/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 
 const Product = require('./models/product');
 
@@ -16,6 +17,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
 
 app.use(express.static(path.join(__dirname, 'views')));
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.set('view engine', 'ejs');
 
@@ -31,7 +33,19 @@ app.get('/products/new', (req, res) => {
 app.post('/products', async (req, res) => {
     const product = new Product(req.body);
     await product.save();
-    res.redirect('/products');
+    res.redirect(`/products/${product._id}`);
+});
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+});
+
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.render('products/edit', { product });
 });
 
 app.get('/products/:id', async (req, res) => {
