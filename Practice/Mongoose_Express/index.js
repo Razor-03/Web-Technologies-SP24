e991e33b70d54888774f1da7a3aa47d4cb28bd3e
Razor-03 <hraza0926@gmail.com>
2024-03-26 22:47:48 +0500

    mongodb connection and model added

--- Practice/Mongoose_Express/index.js
+++ Practice/Mongoose_Express/index.js
@@ -1,6 +1,18 @@
 const express = requier('express');
 const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const Product = require('./models/product');
+
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log('Mongo connection open');
+    })
+    .catch(err => {
+        console.log('Mongo connection error');
+        console.log(err);
+    });
 
 app.use(express.static(path.join(__dirname, 'views')));
 
