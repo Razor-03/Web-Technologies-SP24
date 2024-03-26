b961048c51cdb483896d3100ea2187645d37a074
Razor-03 <hraza0926@gmail.com>
2024-03-27 00:20:18 +0500

    post route created

--- Practice/Mongoose_Express/index.js
+++ Practice/Mongoose_Express/index.js
@@ -15,6 +15,7 @@ mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true,
     });
 
 app.use(express.static(path.join(__dirname, 'views')));
+app.use(express.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs');
 
@@ -27,7 +28,11 @@ app.get('/products/new', (req, res) => {
     res.render('products/new');
 });
 
-
+app.post('/products', async (req, res) => {
+    const product = new Product(req.body);
+    await product.save();
+    res.redirect('/products');
+});
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params;
