75a2a278c096ff73ccd4410b90fae54fa5cb41d6
Razor-03 <hraza0926@gmail.com>
2024-03-27 21:22:34 +0500

    filter by category added to index route

--- Practice/Mongoose_Express/index.js
+++ Practice/Mongoose_Express/index.js
@@ -22,8 +22,14 @@ app.use(methodOverride('_method'));
 app.set('view engine', 'ejs');
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products });
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category });
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category: 'All'});
+    }    
     // res.send(products);
 });
 
