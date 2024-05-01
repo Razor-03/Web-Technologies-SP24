c3bb27acc42c9ae17383327dc5a5fd75231c36cf
Razor-03 <hraza0926@gmail.com>
2024-05-01 18:53:21 +0500

    added scroll behavior

--- EnvisionEstate/client/src/routes/Layout.jsx
+++ EnvisionEstate/client/src/routes/Layout.jsx
@@ -3,11 +3,11 @@ import { Outlet } from "react-router-dom";
 
 export default function Layout() {
     return (
-        <div className="flex flex-col max-w-[100%] md:max-w-[90%] xl:max-w-[90%] md:h-screen mx-auto px-[20px] text-[#0d1321]">
+        <div className="flex flex-col max-w-[100%] md:max-w-[90%] xl:max-w-[90%] h-screen mx-auto px-[20px] text-[#0d1321]">
             <div className="">
                 <Navbar />
             </div>
-            <div className="flex-1">
+            <div className="h-full overflow-hidden">
                 <Outlet />
             </div>
         </div>
--- EnvisionEstate/client/src/routes/Listpage.jsx
+++ EnvisionEstate/client/src/routes/Listpage.jsx
@@ -5,9 +5,9 @@ import { listData } from "../lib/dummyData"
 export default function Listpage() {
     const data = listData;
     return (
-        <div className="flex">
+        <div className="flex h-full">
             <div className="basis-1/2 lg:basis-3/5">
-                <div className="lg:pr-24 flex flex-col gap-20">
+                <div className="lg:pr-24 flex flex-col gap-20 h-full overflow-y-scroll">
                     <Filter/>
                     {data.map(item=>{
                         return <Card key={item.id} item={item}/>
