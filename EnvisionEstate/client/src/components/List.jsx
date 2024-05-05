6f9dff5cebf8ba3aba55fa6adcf4e2c200779658
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-05-06 00:29:26 +0500

    added profile page

--- EnvisionEstate/client/src/components/List.jsx
+++ EnvisionEstate/client/src/components/List.jsx
@@ -1,13 +1,13 @@
 import { listData } from '../lib/dummyData';
-import { Card } from '../components/Card';
+import Card  from '../components/Card';
 
 export default function List() {
     return (
-        <div className="list">
+        <div className="list flex flex-col gap-12">
             {
-                // listData.map(item => {
-                //     return <Card key={item.id} item={item} />
-                // })
+                listData.map(item => {
+                    return <Card key={item.id} item={item} />
+                })
             }
         </div>
     )
