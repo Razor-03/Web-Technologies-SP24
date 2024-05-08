a54532742f37b8b29e3fc5f7f782a3e8667d4b5d
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-05-08 11:54:17 +0500

    added express router

--- EnvisionEstate/backend/routes/posts.js
+++ EnvisionEstate/backend/routes/posts.js
@@ -1,9 +1,8 @@
-const express = require('express');
+import express from "express";
 const router = express.Router();
-
 
 router.get('/', (req, res) => {
     res.send('post route here');
-})
+});
 
-module.exports = router;
+export default router;
