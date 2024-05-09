8671bedde3c7b5b25b3b97cb2fd82366848a9793
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-05-09 21:47:19 +0500

    changed post route to list route

--- assignment 3/app.js
+++ assignment 3/app.js
@@ -15,7 +15,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/posts", postRoutes);
+app.use("/list", postRoutes);
 app.use("/", authRoutes);
 
 app.get("/", (req, res) => {
--- assignment 3/routes/posts.js
+++ assignment 3/routes/posts.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render('posts/index');
+    res.render('list/index');
 });
 
 router.get('/new', (req, res) => {
