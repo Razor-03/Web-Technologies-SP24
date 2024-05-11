b45ffb94a8c42db2b128099dca34318ce8c5193c
Hassan Raza <111640133+Razor-03@users.noreply.github.com>
2024-05-11 18:47:34 +0500

    seed the database initially

---
+++ term-project/seeds/index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const { listData } = require("./seedHelper");
+const Property = require("../models/property");
+
+
+// console.log(listData);
+
+mongoose.connect('mongodb://localhost:27017/envision-estate');
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error'));
+db.once("open", () => {
+    console.log("Databse connected");
+});
+
+const seedDB = async () => {
+    await Property.deleteMany();
+    for (let property of listData) {
+        const estate = new Property(property);
+        await estate.save();
+    }
+}
+
+seedDB().then(console.log("done with that!"));
